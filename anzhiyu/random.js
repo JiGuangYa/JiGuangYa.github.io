08c814b91c37d552286d26eb8deb18d1e75483ff
JiGuangYa <18868617055@163.com>
2023-11-10 18:19:26 +0800

    Site updated: 2023-11-10 18:19:22

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/11/09/hello-world/","2023/11/10/开站大吉/"];function toRandomPost(){
+var posts=["2023/11/10/Hexo建站记录/","2023/11/10/开站大吉/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
