39cf3d6135ced7d44df1225ed9387ab4fd6c96ef
JiGuangYa <18868617055@163.com>
2023-11-12 21:13:42 +0800

    Site updated: 2023-11-12 21:13:37

--- js/custom.js
+++ js/custom.js
@@ -128,3 +128,15 @@ var anzhiyu = {
 anzhiyu.initIndexEssay();
 anzhiyu.changeTimeInEssay();
 anzhiyu.reflashEssayWaterFall();
+
+var bywind = {
+    hideTodayCard: function () {
+        document.getElementById("todayCard") && document.getElementById("todayCard").classList.add("hide")
+    }
+}
+$(".topGroup").hover((function () { }
+), (function () {
+    document.getElementById("todayCard").classList.remove("hide"),
+        document.getElementById("todayCard").style.zIndex = 1
+}
+))
