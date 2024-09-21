29026d6163e5ece98c82ddc9b41f0d6b3d52aa58
JiGuangYa <18868617055@163.com>
2024-09-21 11:59:58 +0800

    Site updated: 2024-09-21 11:59:56

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/20/hello-world.html"];function toRandomPost(){
+var posts=["2024/09/20/hello-world.html","2024/09/21/公告/本站隐私协议.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
